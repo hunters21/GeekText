5eb5e50821bfd68bbc49911c39f47aec565be2ef
hunters21 <92694894+hunters21@users.noreply.github.com>
2022-10-18 16:59:54 -0400

    Delete CommentRepo.java

--- CommentRepo.java
+++
@@ -1,8 +0,0 @@
-package webapp.geektext.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import webapp.geektext.entities.Comment;
-
-public interface CommentRepo extends JpaRepository<Comment, Long> {
-
-}
